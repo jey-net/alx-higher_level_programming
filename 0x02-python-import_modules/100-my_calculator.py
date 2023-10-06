ae08d169b64702375e13ccd8d256666a72621385
Princess <princessjanet1302@gmail.com>
2023-10-05 19:48:48 -0700

    lol

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     if len(argv) != 4:
-        print(:"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
         num1 = int(argv[1])
