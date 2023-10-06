b8d107c54d7745b070b481306fbbac436d79ef83
Princess <princessjanet1302@gmail.com>
2023-10-05 18:08:58 -0700

    pop

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    lists_of_args = []
+
+    if len(argv) == 1:
+        print("0")
+    else:
+        for i in range(1, len(argv)):
+            list_of_args.append(int(argv[i]))
+
+        print(sum(list_of_args))
