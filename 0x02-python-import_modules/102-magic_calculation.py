925912bc87001f6e7bdb5cac7ee493671f2e5072
Princess <princessjanet1302@gmail.com>
2023-10-05 19:08:38 -0700

    loop

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102', globals(), locals(), ['add'], 0).__dict__['add']
+    sub = __import__('magic_calculation_102', globals(), locals(), ['sub'], 0).__dict__['sub']
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+         return c
+    else:
+        return sub(a, b)
