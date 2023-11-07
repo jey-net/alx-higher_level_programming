ee481e4e6c861fe2fc7dad421ad5ad941b2614d2
Princess <princessjanet1302@gmail.com>
2023-11-06 17:52:03 -0800

    new

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-contains the class BaseGeometry
+Contains the class BaseGeometry
 """
 
 
 class BaseGeometry:
-"""A class with public attribute area"""
+    """A class with public attribute area"""
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
